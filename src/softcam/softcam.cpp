0c76039714340ec603e032091c3f8060bf193dbd
Niclas Olmenius <niclas@voysys.se>
2022-08-02 14:10:14 +0200

    register 16 filters

--- src/softcam/softcam.cpp
+++ src/softcam/softcam.cpp
@@ -13,70 +13,51 @@
 
 #define INSTANCE_NAME(number) TEXT_JOIN(CreateSoftcamInstance_, number)
 #define FILTER_NAME(number) TEXT_JOIN(FILTER_NAME_, number)
-#define FILTER_CLSID(number) TEXT_JOIN(CLSID_DShowOdenVirtual_, number)
+#define FILTER_CLSID_NO_PREFIX(number) TEXT_JOIN(DShowOdenVirtual_, number)
 
 #define CREATE_INSTANCE_FUNC(number)                                                                                   \
     CUnknown *WINAPI INSTANCE_NAME(number)(LPUNKNOWN lpunk, HRESULT * phr)                                             \
     {                                                                                                                  \
-        return softcam::Softcam::CreateInstance(FILTER_NAME(number), lpunk, FILTER_CLSID(number), phr);                \
+        return softcam::Softcam::CreateInstance(FILTER_NAME(number), lpunk, __uuidof(FILTER_CLSID_NO_PREFIX(number)),  \
+                                                phr);                                                                  \
     }
 
 #define FILTER_TEMPLATE(number)                                                                                        \
     {                                                                                                                  \
-        FILTER_NAME(number), &FILTER_CLSID(number), &INSTANCE_NAME(number), NULL, nullptr                              \
+        FILTER_NAME(number), &__uuidof(FILTER_CLSID_NO_PREFIX(number)), &INSTANCE_NAME(number), NULL, nullptr          \
     }
 
-#define REGISTER_FILTER(number)                                                                                        \
-    {                                                                                                                  \
-        IFilterMapper2 *pFM2 = nullptr;                                                                                \
-        hr = CoCreateInstance(CLSID_FilterMapper2, nullptr, CLSCTX_INPROC_SERVER, IID_IFilterMapper2, (void **)&pFM2); \
-        if (FAILED(hr))                                                                                                \
-        {                                                                                                              \
-            break;                                                                                                     \
-        }                                                                                                              \
-        pFM2->UnregisterFilter(&CLSID_VideoInputDeviceCategory, 0, FILTER_CLSID(number));                              \
-        hr = pFM2->RegisterFilter(FILTER_CLSID(number), FILTER_NAME(number), 0, &CLSID_VideoInputDeviceCategory,       \
-                                  FILTER_NAME(number), &s_reg_filter2);                                                \
-        pFM2->Release();                                                                                               \
-    }
+class __declspec(uuid("{DA1C0DC0-6A2E-4983-BFB8-D9B446D0D3E2}")) FILTER_CLSID_NO_PREFIX(1);
 
-#define UNREGISTER_FILTER(number)                                                                                      \
-    {                                                                                                                  \
-        IFilterMapper2 *pFM2 = nullptr;                                                                                \
-        hr = CoCreateInstance(CLSID_FilterMapper2, nullptr, CLSCTX_INPROC_SERVER, IID_IFilterMapper2, (void **)&pFM2); \
-        if (FAILED(hr))                                                                                                \
-        {                                                                                                              \
-            break;                                                                                                     \
-        }                                                                                                              \
-        hr = pFM2->UnregisterFilter(&CLSID_VideoInputDeviceCategory, FILTER_NAME(number), FILTER_CLSID(number));       \
-        pFM2->Release();                                                                                               \
-    }
+class __declspec(uuid("{37CF3542-3B79-4E74-88BA-269AA4007FE2}")) FILTER_CLSID_NO_PREFIX(2);
 
-// Generated with GUIDGEN.exe
+class __declspec(uuid("{CBC28B62-FE41-4466-8576-659A2DE09809}")) FILTER_CLSID_NO_PREFIX(3);
 
-// {DA1C0DC0-6A2E-4983-BFB8-D9B446D0D3E2}
-DEFINE_GUID(FILTER_CLSID(1), 0xda1c0dc0, 0x6a2e, 0x4983, 0xbf, 0xb8, 0xd9, 0xb4, 0x46, 0xd0, 0xd3, 0xe2);
+class __declspec(uuid("{81C16FB2-873D-43E8-9697-206EB4E74671}")) FILTER_CLSID_NO_PREFIX(4);
 
-// {37CF3542-3B79-4E74-88BA-269AA4007FE2}
-DEFINE_GUID(FILTER_CLSID(2), 0x37cf3542, 0x3b79, 0x4e74, 0x88, 0xba, 0x26, 0x9a, 0xa4, 0x0, 0x7f, 0xe2);
+class __declspec(uuid("{4EA97013-E144-4392-829A-6DF9370EB943}")) FILTER_CLSID_NO_PREFIX(5);
 
-// {CBC28B62-FE41-4466-8576-659A2DE09809}
-DEFINE_GUID(FILTER_CLSID(3), 0xcbc28b62, 0xfe41, 0x4466, 0x85, 0x76, 0x65, 0x9a, 0x2d, 0xe0, 0x98, 0x9);
+class __declspec(uuid("{5C45B8F9-E720-45FB-8121-7BA1F304A343}")) FILTER_CLSID_NO_PREFIX(6);
 
-// {81C16FB2-873D-43E8-9697-206EB4E74671}
-DEFINE_GUID(FILTER_CLSID(4), 0x81c16fb2, 0x873d, 0x43e8, 0x96, 0x97, 0x20, 0x6e, 0xb4, 0xe7, 0x46, 0x71);
+class __declspec(uuid("{DC9F355A-3C0D-4155-B848-855386D1CC58}")) FILTER_CLSID_NO_PREFIX(7);
 
-// {4EA97013-E144-4392-829A-6DF9370EB943}
-DEFINE_GUID(FILTER_CLSID(5), 0x4ea97013, 0xe144, 0x4392, 0x82, 0x9a, 0x6d, 0xf9, 0x37, 0xe, 0xb9, 0x43);
+class __declspec(uuid("{02226985-7AB8-48A3-BEB1-810637062DFB}")) FILTER_CLSID_NO_PREFIX(8);
 
-// {5C45B8F9-E720-45FB-8121-7BA1F304A343}
-DEFINE_GUID(FILTER_CLSID(6), 0x5c45b8f9, 0xe720, 0x45fb, 0x81, 0x21, 0x7b, 0xa1, 0xf3, 0x4, 0xa3, 0x43);
+class __declspec(uuid("{F2573F2F-ABBD-4EEB-963C-96494776BE84}")) FILTER_CLSID_NO_PREFIX(9);
 
-// {DC9F355A-3C0D-4155-B848-855386D1CC58}
-DEFINE_GUID(FILTER_CLSID(7), 0xdc9f355a, 0x3c0d, 0x4155, 0xb8, 0x48, 0x85, 0x53, 0x86, 0xd1, 0xcc, 0x58);
+class __declspec(uuid("{E4CA698C-A8B2-4DFD-B536-0C0456F87A7C}")) FILTER_CLSID_NO_PREFIX(10);
 
-// {02226985-7AB8-48A3-BEB1-810637062DFB}
-DEFINE_GUID(FILTER_CLSID(8), 0x2226985, 0x7ab8, 0x48a3, 0xbe, 0xb1, 0x81, 0x6, 0x37, 0x6, 0x2d, 0xfb);
+class __declspec(uuid("{6BE35CE4-22D4-4D0A-ADF8-8F64C05DC0EA}")) FILTER_CLSID_NO_PREFIX(11);
+
+class __declspec(uuid("{E98CF627-D302-4854-B0D7-F67B598DA18A}")) FILTER_CLSID_NO_PREFIX(12);
+
+class __declspec(uuid("{B479A9A1-242C-417B-BA36-CC19D1A1C620}")) FILTER_CLSID_NO_PREFIX(13);
+
+class __declspec(uuid("{306C2382-896B-4A88-9C40-6ADED0536972}")) FILTER_CLSID_NO_PREFIX(14);
+
+class __declspec(uuid("{C1D2438C-CE45-4AB6-AA76-46220BAE4FFC}")) FILTER_CLSID_NO_PREFIX(15);
+
+class __declspec(uuid("{C3E15FCB-519F-46AA-A7DB-899B7A9113E8}")) FILTER_CLSID_NO_PREFIX(16);
 
 namespace
 {
@@ -91,6 +72,14 @@ const wchar_t FILTER_NAME_5[] = L"Oden Virtual Webcam 5";
 const wchar_t FILTER_NAME_6[] = L"Oden Virtual Webcam 6";
 const wchar_t FILTER_NAME_7[] = L"Oden Virtual Webcam 7";
 const wchar_t FILTER_NAME_8[] = L"Oden Virtual Webcam 8";
+const wchar_t FILTER_NAME_9[] = L"Oden Virtual Webcam 9";
+const wchar_t FILTER_NAME_10[] = L"Oden Virtual Webcam 10";
+const wchar_t FILTER_NAME_11[] = L"Oden Virtual Webcam 11";
+const wchar_t FILTER_NAME_12[] = L"Oden Virtual Webcam 12";
+const wchar_t FILTER_NAME_13[] = L"Oden Virtual Webcam 13";
+const wchar_t FILTER_NAME_14[] = L"Oden Virtual Webcam 14";
+const wchar_t FILTER_NAME_15[] = L"Oden Virtual Webcam 15";
+const wchar_t FILTER_NAME_16[] = L"Oden Virtual Webcam 16";
 
 const AMOVIESETUP_MEDIATYPE s_pin_types[] = {{
     &MEDIATYPE_Video,  // Major type
@@ -119,16 +108,63 @@ CREATE_INSTANCE_FUNC(5)
 CREATE_INSTANCE_FUNC(6)
 CREATE_INSTANCE_FUNC(7)
 CREATE_INSTANCE_FUNC(8)
+CREATE_INSTANCE_FUNC(9)
+CREATE_INSTANCE_FUNC(10)
+CREATE_INSTANCE_FUNC(11)
+CREATE_INSTANCE_FUNC(12)
+CREATE_INSTANCE_FUNC(13)
+CREATE_INSTANCE_FUNC(14)
+CREATE_INSTANCE_FUNC(15)
+CREATE_INSTANCE_FUNC(16)
 
 } // namespace
 
 // COM global table of objects in this dll
 
 CFactoryTemplate g_Templates[] = {
-    FILTER_TEMPLATE(1), FILTER_TEMPLATE(2), FILTER_TEMPLATE(3), FILTER_TEMPLATE(4),
-    FILTER_TEMPLATE(5), FILTER_TEMPLATE(6), FILTER_TEMPLATE(7), FILTER_TEMPLATE(8),
+    FILTER_TEMPLATE(1),  FILTER_TEMPLATE(2),  FILTER_TEMPLATE(3),  FILTER_TEMPLATE(4),
+    FILTER_TEMPLATE(5),  FILTER_TEMPLATE(6),  FILTER_TEMPLATE(7),  FILTER_TEMPLATE(8),
+    FILTER_TEMPLATE(9),  FILTER_TEMPLATE(10), FILTER_TEMPLATE(11), FILTER_TEMPLATE(12),
+    FILTER_TEMPLATE(13), FILTER_TEMPLATE(14), FILTER_TEMPLATE(15), FILTER_TEMPLATE(16),
 };
+
 int g_cTemplates = sizeof(g_Templates) / sizeof(g_Templates[0]);
+
+static_assert(sizeof(g_cTemplates == sizeof(g_Templates[0]) * 16), "g_Templates should contain 16 filter definitions");
+
+void register_filter(int N)
+{
+    const WCHAR *filter_name = g_Templates[N].m_Name;
+    const CLSID filter_clsid = *g_Templates[N].m_ClsID;
+
+    IFilterMapper2 *pFM2 = nullptr;
+    HRESULT hr =
+        CoCreateInstance(CLSID_FilterMapper2, nullptr, CLSCTX_INPROC_SERVER, IID_IFilterMapper2, (void **)&pFM2);
+    if (FAILED(hr))
+    {
+        return;
+    }
+    pFM2->UnregisterFilter(&CLSID_VideoInputDeviceCategory, 0, filter_clsid);
+    hr = pFM2->RegisterFilter(filter_clsid, filter_name, 0, &CLSID_VideoInputDeviceCategory, filter_name,
+                              &s_reg_filter2);
+    pFM2->Release();
+}
+
+void unregister_filter(int N)
+{
+    const WCHAR *filter_name = g_Templates[N].m_Name;
+    const CLSID filter_clsid = *g_Templates[N].m_ClsID;
+
+    IFilterMapper2 *pFM2 = nullptr;
+    HRESULT hr =
+        CoCreateInstance(CLSID_FilterMapper2, nullptr, CLSCTX_INPROC_SERVER, IID_IFilterMapper2, (void **)&pFM2);
+    if (FAILED(hr))
+    {
+        return;
+    }
+    hr = pFM2->UnregisterFilter(&CLSID_VideoInputDeviceCategory, filter_name, filter_clsid);
+    pFM2->Release();
+}
 
 STDAPI DllRegisterServer()
 {
@@ -144,16 +180,10 @@ STDAPI DllRegisterServer()
     }
     do
     {
-
-        REGISTER_FILTER(1);
-        REGISTER_FILTER(2);
-        REGISTER_FILTER(3);
-        REGISTER_FILTER(4);
-        REGISTER_FILTER(5);
-        REGISTER_FILTER(6);
-        REGISTER_FILTER(7);
-        REGISTER_FILTER(8);
-
+        for (int i = 0; i < 16; i++)
+        {
+            register_filter(i);
+        }
     } while (0);
     CoFreeUnusedLibraries();
     CoUninitialize();
@@ -174,16 +204,10 @@ STDAPI DllUnregisterServer()
     }
     do
     {
-
-        UNREGISTER_FILTER(1);
-        UNREGISTER_FILTER(2);
-        UNREGISTER_FILTER(3);
-        UNREGISTER_FILTER(4);
-        UNREGISTER_FILTER(5);
-        UNREGISTER_FILTER(6);
-        UNREGISTER_FILTER(7);
-        UNREGISTER_FILTER(8);
-
+        for (int i = 0; i < 16; i++)
+        {
+            unregister_filter(i);
+        }
     } while (0);
     CoFreeUnusedLibraries();
     CoUninitialize();
